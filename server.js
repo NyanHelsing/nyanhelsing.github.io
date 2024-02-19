fc04be2beb683b83d36e6775ab4dc31b1b09c60f
deathg.rip <josh@deathg.rip>
2024-02-19 03:23:42 -0500

    adds privacy policy

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ Deno.serve(({
     url
 }) => {
     const path = new URL(url, import.meta.url).pathname;
+
+    if (path === "/intake") {
+         
+        
+    }
+
     return new Response(`Hello, path is ${path}`);
 });
