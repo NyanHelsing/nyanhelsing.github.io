f4898220c003d2eb7234ea193bc033b8b2612d7b
deathg.rip <josh@deathg.rip>
2024-03-06 17:06:13 -0500

    Fix reak hook inconsistency

--- src/social-media-links.jsx
+++ src/social-media-links.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 
-export const SocialMediaLinks = () => (
-    <>
-        <h2>Links</h2>
-        <nav className="social">
-            <ol>
-                <li>
-                    <a href="https://github.com/nyanhelsing">
-                        <i className="fa-brands fa-github" />
-                        Nyan Helsing's GitHub Profile
-                    </a>
-                </li>
-                <li>
-                    <a href="https://discord.gg/M9Kp94ktKb">
-                        <i className="fa-brands fa-discord" />
-                        The Hunters - Nyan Helsing's Discord Server.
-                    </a>
-                </li>
-            </ol>
-        </nav>
-    </>
-);
+import { useLocator } from "./locator";
+
+const MOUNT_PATHS = new Set(["/index.html", "/index.htm", "/index", "/"]);
+export const SocialMediaLinks = () => {
+    const [locator] = useLocator();
+    if (MOUNT_PATHS.has(locator.pathname)) {
+        return (
+            <>
+                <h2>Links</h2>
+                <nav className="social">
+                    <ol>
+                        <li>
+                            <a href="https://github.com/nyanhelsing">
+                                <i className="fa-brands fa-github" />
+                                Nyan Helsing's GitHub Profile
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://discord.gg/M9Kp94ktKb">
+                                <i className="fa-brands fa-discord" />
+                                The Hunters - Nyan Helsing's Discord Server.
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </>
+        );
+    }
+};
--- src/tools.jsx
+++ src/tools.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 import { useLocator } from "./locator.jsx";
 
+const MOUNT_PATHS = new Set(["/tools.html", "/tools"]);
 export const Tools = () => {
     const [locator] = useLocator();
     const openedMailto = useRef(false);
@@ -11,15 +12,8 @@ export const Tools = () => {
     const bodyRef = useRef();
     const [mailto, setMailto] = useState("");
 
-    if (!new Set(["/tools.html", "/tools"]).has(locator.pathname)) {
-        return undefined;
-    }
-
-    const portableMailto = encodeURIComponent(btoa(mailto));
-
-    const mailtoParam = new URLSearchParams(locator.search).get("mailto");
-
     useEffect(() => {
+        const mailtoParam = new URLSearchParams(locator.search).get("mailto");
         if (mailtoParam && !openedMailto.current) {
             const mailto = atob(decodeURIComponent(mailtoParam));
             window.location.href = mailto;
@@ -30,72 +24,75 @@ export const Tools = () => {
             bodyRef.current.value = mailtoUrl.searchParams.get("body");
             setMailto(mailto);
         }
-    }, [mailtoParam]);
+    }, [locator]);
+
+    const portableMailto = encodeURIComponent(btoa(mailto));
 
     return (
-        <>
-            <h1>Tools</h1>
+        MOUNT_PATHS.has(locator.pathname) && (
+            <>
+                <h1>Tools</h1>
 
-            {mailtoParam && (
-                <div>
-                    <h2>You've Been sent a Portable Mailto Link</h2>
-                    <p>
-                        The link should have opened automatically. If it didn't, it might because a
-                        popup blocker blocked it. click the button below to open it.
-                    </p>
+                {mailtoParam && (
+                    <div>
+                        <h2>You've Been sent a Portable Mailto Link</h2>
+                        <p>
+                            The link should have opened automatically. If it didn't, it might
+                            because a popup blocker blocked it. click the button below to open it.
+                        </p>
+                        <button
+                            onClick={() => {
+                                const mailto = atob(decodeURIComponent(mailtoParam));
+                                window.location.href = mailto;
+                                openedMailto.current = true;
+                            }}
+                        >
+                            Open mailto link
+                        </button>
+                    </div>
+                )}
+
+                <h2>mailto generator</h2>
+                <form>
+                    <label>
+                        Email address:
+                        <input type="email" ref={emailRef} />
+                    </label>
+                    <label>
+                        Subject:
+                        <input type="text" ref={subjectRef} />
+                    </label>
+                    <label>
+                        Body:
+                        <textarea ref={bodyRef} />
+                    </label>
                     <button
-                        onClick={() => {
-                            const mailto = atob(decodeURIComponent(mailtoParam));
-                            window.location.href = mailto;
-                            openedMailto.current = true;
+                        type="submit"
+                        onClick={(event) => {
+                            event.preventDefault();
+                            // need to encode the subject and body
+                            const email = emailRef.current.value;
+                            const subject = encodeURIComponent(subjectRef.current.value);
+                            const body = encodeURIComponent(bodyRef.current.value);
+                            setMailto(`mailto:${email}?subject=${subject}&body=${body}`);
                         }}
                     >
-                        Open mailto link
+                        Generate mailto link
                     </button>
-                </div>
-            )}
-
-            <h2>mailto generator</h2>
-            <form>
-                <label>
-                    Email address:
-                    <input type="email" ref={emailRef} />
-                </label>
-                <label>
-                    Subject:
-                    <input type="text" ref={subjectRef} />
-                </label>
-                <label>
-                    Body:
-                    <textarea ref={bodyRef} />
-                </label>
-                <button
-                    type="submit"
-                    onClick={(event) => {
-                        event.preventDefault();
-                        // need to encode the subject and body
-                        const email = emailRef.current.value;
-                        const subject = encodeURIComponent(subjectRef.current.value);
-                        const body = encodeURIComponent(bodyRef.current.value);
-                        setMailto(`mailto:${email}?subject=${subject}&body=${body}`);
-                    }}
-                >
-                    Generate mailto link
-                </button>
-                <output>
-                    <h3>Mailto link</h3>
-                    <pre>{mailto}</pre>
-                    <h3>Mailto anchor tag (clicking this takes you to the mailto directly)</h3>
-                    <a href={mailto}>mailto link</a>
-                    <h3>Portable mailto link</h3>
-                    <p>
-                        this link enables the mailto to be used in apps like linktree that might not
-                        directly support the <pre>mailto:</pre>
-                        protocol
-                    </p>
-                    <pre>https://nyanhelsing.github.io/tools.html?mailto={portableMailto}</pre>
-                </output>
-            </form>
-        </>
+                    <output>
+                        <h3>Mailto link</h3>
+                        <pre>{mailto}</pre>
+                        <h3>Mailto anchor tag (clicking this takes you to the mailto directly)</h3>
+                        <a href={mailto}>mailto link</a>
+                        <h3>Portable mailto link</h3>
+                        <p>
+                            This link enables the mailto to be used in apps like linktree that might
+                            not directly support the <i>"mailto"</i> protocol.
+                        </p>
+                        <pre>https://nyanhelsing.github.io/tools.html?mailto={portableMailto}</pre>
+                    </output>
+                </form>
+            </>
+        )
     );
 };
