c04015647542305d072b7095e5b5071d4d5bf0ed
deathg.rip <josh@deathg.rip>
2024-03-07 10:55:03 -0500

    Deploy: vundefined

--- server.js
+++
@@ -1,8 +0,0 @@
-Deno.serve(({ url }) => {
-    const path = new URL(url, import.meta.url).pathname;
-
-    if (path === "/intake") {
-    }
-
-    return new Response(`Hello, path is ${path}`);
-});
