2d6633ef283ac0f921bac01024d1e2c992469bbf
deathg.rip <josh@deathg.rip>
2024-03-25 05:12:31 -0400

    Deploy: v1.1.25

---
+++ assets/terms-BIcQAt-7.js
@@ -0,0 +1,1 @@
+import"./entry-BzRoo7eq.js";import i from"https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";globalThis.config={get:_config.get.bind(_config)};i.initialize({startOnLoad:!0});window.mermaid=i;
